1d56fd3fec482789db2d18d0624760628f5b2171
mrodionov <Sweeroll@gmail.com>
2021-10-13 16:29:32 +0300

    Some fixes

--- pages/login_page.py
+++ pages/login_page.py
@@ -45,7 +45,8 @@ class LoginPage(BasePage):
         return self.find_element(BasePageLocators.CONFIRM_EXIT_BUTTON)
 
     def auth(self, data: AuthData):
-        logger.info(f'User login is "{data.login}, user password {data.password}"')
+        logger.info(f'Login is {data.login}\n,'
+                    f'Password {data.password}\n')
         if self.is_auth():
             self.click_element(self.user_menu())
             self.click_element(self.exit())
